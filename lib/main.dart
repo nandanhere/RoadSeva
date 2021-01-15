7e0219f7b8d2d0b47c2f8beed4dac56a32e17744
TheNova22 <jayantsogikar@gmail.com>
2021-01-15 23:31:00 +0530

    Added the required files

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:road_seva/screens/auth_screen.dart';
 import 'package:road_seva/screens/complaint_register.dart';
 import 'package:road_seva/screens/pothole_Details.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:road_seva/screens/show_all_near_me.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -16,7 +21,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.hasData) {
+            return PotHolesNearMe();
+          }
+          return AuthScreen();
+        },
+      ),
       routes: {
         ComplaintRegisterScreen.routeName: (ctx) => ComplaintRegisterScreen(),
         PotholeDetails.routeName: (ctx) => PotholeDetails()
