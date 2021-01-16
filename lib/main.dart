968b023da06aa26203dfd9a3cb8a764de4488d9a
TheNova22 <jayantsogikar@gmail.com>
2021-01-16 15:09:39 +0530

    Synced everything

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 import 'package:road_seva/screens/auth_screen.dart';
 import 'package:road_seva/screens/complaint_register.dart';
 import 'package:road_seva/screens/pothole_Details.dart';
@@ -13,11 +14,12 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "RoadSeva",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
