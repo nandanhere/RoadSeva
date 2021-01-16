ba06d6dd339160f14405668f83eb35483ed72593
Nandan <nanspyro@gmail.com>
2021-01-16 15:00:16 +0530

    Maps extended

--- lib/screens/complaint_register.dart
+++ lib/screens/complaint_register.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:road_seva/models/place.dart';
 import 'package:road_seva/screens/map_screen.dart';
-import 'package:road_seva/widgets/place_input.dart';
 
 import 'package:flutter/material.dart';
 import 'package:road_seva/widgets/image_input.dart';
@@ -21,6 +20,7 @@ class ComplaintRegisterScreen extends StatefulWidget {
 class _ComplaintRegisterScreenState extends State<ComplaintRegisterScreen> {
   File _pickedImage;
   PotHoleDetails _pickedLocation;
+
   void _selectImage(File pickedImage) {
     _pickedImage = pickedImage;
   }
@@ -39,57 +39,54 @@ class _ComplaintRegisterScreenState extends State<ComplaintRegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Register a complaint"),
-      ),
-      body: MapScreen(
-        latitude: 30.00,
-        longitude: 40.00,
-        isSelecting: true,
-      ),
-      // Column(
-      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //   crossAxisAlignment: CrossAxisAlignment.stretch,
-      //   children: [
-      //     Expanded(
-      //         child: SingleChildScrollView(
-      //       child: Padding(
-      //         padding: const EdgeInsets.all(10.0),
-      //         child: MapScreen(
-      //           latitude: 30.00,
-      //           longitude: 40.00,
-      //           isSelecting: true,
-      //         ),
-      //         Column(
-      //           children: [
-      //             SizedBox(
-      //               height: 10,
-      //             ),
-      //             ImageInput(
-      //               onImageSelect: _selectImage,
-      //             ),
-      //             SizedBox(
-      //               height: 10,
-      //             ),
-      //             LocationInput(
-      //               onSelectPlace: _selectPlace,
-      //             )
-      //           ],
-      //         ),
-      // ),
-      // ),
-      // ),\
-      // RaisedButton.icon(
-      //   elevation: 0,
-      //   materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-      //   color: Theme.of(context).accentColor,
-      //   icon: Icon(Icons.add),
-      //   label: Text("Add Place!"),
-      //   onPressed: _savePlace,
-      // )
-      //   ],
-      // ),
+    final args = ModalRoute.of(context).settings.arguments;
+    print(args);
+    return MapScreen(
+      latitude: 30.00,
+      longitude: 40.00,
+      isSelecting: true,
     );
+    // Column(
+    //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //   crossAxisAlignment: CrossAxisAlignment.stretch,
+    //   children: [
+    //     Expanded(
+    //         child: SingleChildScrollView(
+    //       child: Padding(
+    //         padding: const EdgeInsets.all(10.0),
+    //         child: MapScreen(
+    //           latitude: 30.00,
+    //           longitude: 40.00,
+    //           isSelecting: true,
+    //         ),
+    //         Column(
+    //           children: [
+    //             SizedBox(
+    //               height: 10,
+    //             ),
+    //             ImageInput(
+    //               onImageSelect: _selectImage,
+    //             ),
+    //             SizedBox(
+    //               height: 10,
+    //             ),
+    //             LocationInput(
+    //               onSelectPlace: _selectPlace,
+    //             )
+    //           ],
+    //         ),
+    // ),
+    // ),
+    // ),\
+    // RaisedButton.icon(
+    //   elevation: 0,
+    //   materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    //   color: Theme.of(context).accentColor,
+    //   icon: Icon(Icons.add),
+    //   label: Text("Add Place!"),
+    //   onPressed: _savePlace,
+    // )
+    //   ],
+    // ),
   }
 }
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:road_seva/models/place.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   final double latitude, longitude;
@@ -13,6 +13,9 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LocationData locData;
+  LatLng d = LatLng(0, 0);
+
   LatLng _pickedLocation;
   void _selectLocation(LatLng position) {
     setState(() {
@@ -22,18 +25,38 @@ class _MapScreenState extends State<MapScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: CameraPosition(
-          zoom: 16, target: LatLng(widget.latitude, widget.longitude)),
-      onTap: widget.isSelecting ? _selectLocation : null,
-      markers: (_pickedLocation == null && widget.isSelecting)
-          ? {}
-          : {
-              Marker(
-                  markerId: MarkerId("m1"),
-                  position: _pickedLocation ??
-                      LatLng(widget.latitude, widget.longitude)),
-            },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register a complaint"),
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
+            initialCameraPosition: CameraPosition(
+                zoom: 16, target: LatLng(widget.latitude, widget.longitude)),
+            onTap: widget.isSelecting ? _selectLocation : null,
+            markers: (_pickedLocation == null && widget.isSelecting)
+                ? {}
+                : {
+                    Marker(
+                        markerId: MarkerId("m1"),
+                        position: _pickedLocation ??
+                            LatLng(widget.latitude, widget.longitude)),
+                  },
+          ),
+          Container(
+            child: Row(
+              children: [
+                Image.network(""),
+                IconButton(icon: Icon(Icons.camera), onPressed: () {}),
+                FlatButton(onPressed: () {}, child: Text("Report complaint")),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
